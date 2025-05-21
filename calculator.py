b48851be0b553770233a93e011b48fae7a8110f2
ChrisB2024 <159099695+ChrisB2024@users.noreply.github.com>
2025-05-21 07:10:15 -0400

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,35 +1,22 @@
-x = int(input("Enter a number: " ))
-y = int(input("Enter another number: "))
-user_input = input("Enter an operation: ")
-addition = x + y
-substraction = x - y
-multiplication = x * y
-exponentiation = x ** y
-if user_input == "+":
-    print(addition)
-elif user_input == "-":
-    print(substraction)
-elif user_input == "*":
-    print(multiplication)
-elif user_input == "/":
-        try:
-            division =  x / y
-            print(division)
-        except ZeroDivisionError:
-            print("Error: Division by zero")
-elif user_input == "**":
-    print(exponentiation)
-elif user_input == "//":
-        try:
-            floor_division = x // y
-            print(floor_division)
-        except ZeroDivisionError:
-            print("Error: Division by zero")
-elif user_input == "%":
-    try:
-        modulus = x % y
-        print(modulus)
-    except ZeroDivisionError:
-        print("Error: Division by zero")
-else:
-    print("Invalid operation")
+try:
+    x = int(input("Enter a number: " ))
+    y = int(input("Enter another number: "))
+    user_input = input("Enter an operation: ")
+    if user_input == "+":
+         print(x + y)
+    elif user_input == "-":
+         print(x - y)
+    elif user_input == "*":
+         print(x * y)
+    elif user_input == "/":
+         print(x/y)
+    elif user_input == "**":
+         print(x ** y)
+    elif user_input == "//":
+         print(x // y)
+    elif user_input == "%":
+         print(x % y)
+except ZeroDivisionError:
+    print("Error: Division by zero")
+except ValueError:
+    print("Error: Invalid input")
